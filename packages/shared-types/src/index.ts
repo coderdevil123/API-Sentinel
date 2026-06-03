a24b9a2f12ddd8e8f953b5ba997a55d9721ce197
coderdevil123 <shubhangmishra094@gmail.com>
2026-06-03 14:48:57 +0530

    feat(shared-types): add core domain interfaces

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,8 @@
+export * from "./enums/severity.enum";
+export * from "./enums/vulnerability-category.enum";
+
+export * from "./interfaces/evidence.interface";
+
+export * from "./findings/finding.interface";
+
+export * from "./scans/scan.interface";
